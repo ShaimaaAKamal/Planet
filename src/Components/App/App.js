9cabfa6c42fb7301c42aaee917c490f325c9f1f6
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-25 20:32:34 +0200

    add app routes and add outlet

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import NotFound from "../NotFound/NotFound";
+import Planet from "../Planet/Planet";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const routes=createBrowserRouter([
+    {path:'/' ,element:<Layout/>,errorElement:<NotFound/>,children:[
+      {index:true ,element:<Navigate to='/Planet/Mercury'/>},
+      {path:'Planet/:planet',element:<Planet/>}
+    ]}
+  ])
   return (
     <div className="App">
-      <h1>App</h1>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
