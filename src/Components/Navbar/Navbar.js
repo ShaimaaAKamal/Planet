1dadd35f90a9de57009a0e7a192633ae000dc654
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-25 22:42:09 +0200

    implement navbar component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function Navbar() {
+export default function Navbar({planetsData}) {
+    const navigate=useNavigate();
+    const planets=planetsData.map(planet =>  planet.name);
+    const backGrounds=['#D9F5FD','#FFCA72','#525BFF','#FF5C34','#F7AA71','#FFC957','#00F4D4','#3480FF'];
+        // let [image,setImage]=useState('');
+
+    // const getImage=async(planet)=>{
+    //     const planetInfo=planetsData.find(plan => plan.name === planet)
+    //     const planetLogo=await import(`./../../assets${planetInfo.images.planet}`)
+    //     setImage(planetLogo.default)
+    // }
+    const getPlanet=(planet)=>{
+       navigate(`/Planet/${planet}`);
+    }
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+    <div className="container-fluid">
+      <Link className="navbar-brand text-uppercase" to="/">The Planets</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fillRule="evenodd">
+            <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g>
+        </svg>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+          {planets.map((planet,index)=> 
+              (<li className="nav-item" key={index} >
+             <div className='d-flex justify-content-between align-items-center py-1 Planet' onClick={()=>getPlanet(planet)}>
+                <div className='d-flex align-items-center'>
+                   <i className="fa-solid fa-circle d-md-none me-3" style={{color:`${backGrounds[index]}`}}></i>
+                    <Link className="nav-link fw-semiBold text-uppercase" aria-current="page" to={`/Planet/${planet}`}>{planet}</Link>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" className='d-md-none'>
+                    <path fill="none" stroke="#FFF" opacity=".4" d="M1 0l4 4-4 4"/>
+                </svg>
+             </div>
+          </li>)
+          
+          )}
+        </ul>
+      </div>
+    </div>
+  </nav>
   )
 }
