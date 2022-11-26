26979a1dfa1f644d6dc7090bc28c28660cdd4614
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-26 17:44:25 +0200

    change background colors

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,15 +4,17 @@ import NotFound from "../NotFound/NotFound";
 import Planet from "../Planet/Planet";
 import { Navigate } from "react-router-dom";
 import planetsData from '../../data.json';
-
 function App() {
-  const backGrounds=['#D9F5FD','#FFCA72','#525BFF','#FF5C34','#F7AA71','#FFC957','#00F4D4','#3480FF'];
+  const backGrounds=['#419EBB','#EDA249','#6f2ed6','#D14C32','#D83A34','#CD5120','#1ec2a4','#2d68f0'];
   const routes=createBrowserRouter([
     {path:'/' ,element:<Layout planetsData={planetsData} backGrounds={backGrounds}/>,errorElement:<NotFound/>,children:[
       {index:true ,element:<Navigate to='/Planet/Mercury'/>},
       {path:'Planet/:planet',element:<Planet planetsData={planetsData}/>}
     ]}
   ])
+
+ 
+ 
   return (
     <div className="App">
       <RouterProvider router={routes}></RouterProvider>
