d69ad1f112d610c0f5a76214e36454938874f142
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-26 17:00:00 +0200

    update Notfound Implemenation and styling

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import NotFoundImage from '../../assets/404.png'
+import { Link } from 'react-router-dom'
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='vh-100 d-flex align-items-center justify-content-center position-relative' >
+      <Link to='/'>
+        <span className='position-absolute rounded-2 NotFoundLink'>
+        <i className="fa-solid fa-arrow-left me-2"></i>Go Home</span>
+      </Link>
+      <img src={NotFoundImage} alt="Not found image"  className='w-100 h-100' style={{objectFit:'cover'}}/>
+    </div>
   )
 }
