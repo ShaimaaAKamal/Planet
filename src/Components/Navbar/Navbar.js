fc5a0496276e595b625d338907cde992c6cbbcd5
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-26 16:23:37 +0200

    adjust nav layout

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,9 +8,9 @@ export default function Navbar({planetsData,backGrounds}) {
        navigate(`/Planet/${planet}`);
     }
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+    <nav className="navbar navbar-expand-lg navbar-dark  px-3 py-3">
     <div className="container-fluid">
-      <Link className="navbar-brand text-uppercase antoFamily" to="/">The Planets</Link>
+      <Link className="navbar-brand text-uppercase antoFamily mb-md-3 mb-lg-0 fs-2" to="/">The Planets</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fillRule="evenodd">
             <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g>
@@ -23,7 +23,7 @@ export default function Navbar({planetsData,backGrounds}) {
              <div className='d-flex justify-content-between align-items-center py-1 Planet' onClick={()=>getPlanet(planet)}>
                 <div className='d-flex align-items-center'>
                    <i className="fa-solid fa-circle d-md-none me-3" style={{color:`${backGrounds[index]}`}}></i>
-                    <Link className="nav-link fw-semiBold text-uppercase" aria-current="page" to={`/Planet/${planet}`}>{planet}</Link>
+                    <Link className="nav-link fw-semiBold fs-7 text-uppercase" aria-current="page" to={`/Planet/${planet}`}>{planet}</Link>
                 </div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" className='d-md-none'>
                     <path fill="none" stroke="#FFF" opacity=".4" d="M1 0l4 4-4 4"/>
