abcd6d3dd09800fa00339f98fe33a7d2ab010a6a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-26 16:23:04 +0200

    Implement sciinfo Component

--- src/Components/SciInfo/SciInfo.jsx
+++ src/Components/SciInfo/SciInfo.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
-export default function SciInfo() {
+export default function SciInfo({planet}) {
   return (
-    <div>SciInfo</div>
+    <div className='container-lg mb-5 mt-lg-3 px-5 px-md-4 px-lg-0'>
+       <div className="row gy-3 gx-2">
+          <div className="col-md-3">
+             <div className='border border-1 border-secondary p-4'>
+                <span className='small text-muted fw-semibold'>ROTATION TIME</span>
+                <h3 className='mt-3 facts'>{planet.rotation}</h3>
+             </div>
+          </div>
+          <div className="col-md-3">
+             <div className='border border-1 border-secondary p-4'>
+                <span className='small text-muted fw-semibold'>REVOLUTION TIME</span>
+                <h3 className='mt-3 facts'>{planet.revolution}</h3>
+             </div>
+          </div>
+          <div className="col-md-3">
+             <div className='border border-1 border-secondary p-4'>
+                <span className='small text-muted fw-semibold'>Radius</span>
+                <h3 className='mt-3 facts'>{planet.radius}</h3>
+             </div>
+          </div>
+          <div className="col-md-3">
+             <div className='border border-1 border-secondary p-4'>
+                <span className='small text-muted fw-semibold'>AVERAGE TEMP</span>
+                <h3 className='mt-3 facts'>{planet.temperature}</h3>
+             </div>
+          </div>
+       </div>
+    </div>
   )
 }
