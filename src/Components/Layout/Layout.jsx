fa412ee064c790e8e402b8a9746c62c9ca32c800
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-26 17:45:04 +0200

    add logic to display activelink

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../Navbar/Navbar'
+import Navbar from '../Navbar/Navbar';
+import { useEffect } from "react";
 
 export default function Layout({planetsData,backGrounds}) {
+  useEffect(()=>{
+    const navLinks=document.querySelectorAll('.nav-link');
+    const NavColors=['#419EBB','#EDA249','#6f2ed6','#D14C32','#D83A34','#CD5120','#1ec2a4','#2d68f0']
+    navLinks.forEach((link,index) => {
+      if(link.classList.contains('active')){
+        link.style.setProperty('--navLinkColor',NavColors[index] )
+      }
+      link.addEventListener('click',function(e){
+        link.style.setProperty('--navLinkColor',NavColors[index] )
+      })
+    })
+},[])
   return (
     <>
       <Navbar planetsData={planetsData} backGrounds={backGrounds}/>
